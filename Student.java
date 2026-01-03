163749968b46b55721059511c31a6899e643d155
smrithiboorgula <smrithiboorgula06@gmail.com>
2026-01-03 11:04:02 +0530

    Student

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    String name;
+    int age;
+    Student(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    public static void main(String[] args) {
+        Student obj = new Student("Smrithi", 19);
+        System.out.println(obj.name+" "+obj.age);
+    }
+}
