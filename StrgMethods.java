b8f4c0f32aa1b71ae6acef03a6056ca0c2c39db4
smrithiboorgula <smrithiboorgula06@gmail.com>
2026-01-03 09:19:38 +0530

    String Methods

---
+++ StrgMethods.java
@@ -0,0 +1,15 @@
+public class StrgMethods {
+    public static void main(String[] args){
+      /*   String s = "Computer Science Engineer";
+        s =s.concat(" Delta");
+        System.out.println(s);
+        */
+
+       /* String s = "CSE DELTA";
+       System.out.println(s.substring(4));
+       */
+
+        String s ="Computer Science And Engineering DELTA";
+        System.out.println(s.substring(0, 16));
+    }
+}
